34896806da195ec28fee9b1b02664d49a3f70245
mojtabamza <mojtaba.zamani.65@gmail.com>
2024-07-24 00:42:36 +0200

    initial commit

---
+++ Project_4/README.md
@@ -0,0 +1,1 @@
+# A Strategy Pattern Project
---
+++ Project_4/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+
+
+
+int main(void) {
+    
+    cout << "Strategy Pattern Project" << endl;
+    
+    return 0;
+}
