2701c218a23aa89603f7db29173cf8436b55e517
mojtabamza <mojtaba.zamani.65@gmail.com>
2024-07-24 01:52:54 +0200

    initial commit

---
+++ Project_5/README.md
@@ -0,0 +1,2 @@
+# A Decorator Pattern Project
+
---
+++ Project_5/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+
+
+
+int main(void) {
+
+    cout << "Decorator Pattern Project" << endl;
+    
+    
+    
+    return 0;
+}
