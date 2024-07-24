a575b893d86db297763ea5f447085c5e231c118a
mojtabamza <mojtaba.zamani.65@gmail.com>
2024-07-25 01:50:14 +0200

    updated the Decorator Pattern

--- Project_5/src/DecoratorPattern.hpp
+++ Project_5/src/DecoratorPattern.hpp
@@ -1,52 +1,53 @@
-#pragma once
+#include <iostream>
 #include <string>
 
 using std::string;
+using std::cout;
+using std::endl;
 
-//Componnent
+// Component (Abstract Coffee)
 class Coffee_t {
-    public:
+public:
     virtual ~Coffee_t() {}
-    virtual string Type(void) const = 0;
-    virtual string Cost(void) const = 0;
+    virtual string Type() const = 0;
+    virtual string Cost() const = 0;
 };
 
-//Concrete Componnent
+// Concrete Component (Espresso)
 class Espresso_t : public Coffee_t {
-    public:
-    string Type(void) const override {
+public:
+    string Type() const override {
         return "Espresso!";
     }
-    string Cost(void) const override {
+    string Cost() const override {
         return "20 Euro!";
     }
 };
 
-//Decorator
+// Decorator
 class Decorator_t : public Coffee_t {
-    public:
-    Decorator_t(Coffee_t* coffeeObj) : coffeeObj(coffeeObj) {}
-    string Type(void) const override {
-        return this->coffeeObj->Type();
-    }
-    string Cost(void) const override {
-        return this->coffeeObj->Cost();
-    }
-    private:
+protected:
     Coffee_t* coffeeObj;
+
+public:
+    Decorator_t(Coffee_t* coffeeObj) : coffeeObj(coffeeObj) {}
+    string Type() const override {
+        return coffeeObj->Type();
+    }
+    string Cost() const override {
+        return coffeeObj->Cost();
+    }
 };
 
-//Concrete Decorator
+// Concrete Decorator (Milk)
 class Milk_t : public Decorator_t {
-    public:
+public:
     Milk_t(Coffee_t* coffeeObj) : Decorator_t(coffeeObj) {}
-    string Type(void) const override {
-        return this->coffeeObj->Type() + "Milk";
+    string Type() const override {
+        return coffeeObj->Type() + " Milk";
     }
-    string Cost(void) const override {
-        return this->coffeeObj->Cost() + "10 Euro";
+    string Cost() const override {
+        return coffeeObj->Cost() + " + 10 Euro";
     }
-    private:
-    Coffee_t* coffeeObj;
 };
 
