02481bbb2e130d43b5a0c745f2011cffbd421015
mojtabamza <mojtaba.zamani.65@gmail.com>
2024-07-25 01:31:49 +0200

    updated README and modified main to run

--- Project_5/README.md
+++ Project_5/README.md
@@ -1,2 +1,3 @@
 # A Decorator Pattern Project
 
+# The Decorator Pattern in C++ is a structural design pattern that allows you to dynamically add new behaviors or responsibilities to individual objects without altering their underlying class structure.
--- Project_5/src/main.cpp
+++ Project_5/src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include "DecoratorPattern.hpp"
 
 
 using std::cout;
 using std::endl;
 
 
-
-int main(void) {
-
+int main() {
     cout << "Decorator Pattern Project" << endl;
-    
-    
-    
+
+    Coffee_t* newCoffee = new Espresso_t();
+    cout << newCoffee->Type() << "\t" << newCoffee->Cost() << endl;
+
+    Coffee_t* newCoffeeMilk = new Milk_t(new Espresso_t());
+    cout << newCoffeeMilk->Type() << "\t" << newCoffeeMilk->Cost() << endl;
+
+    delete newCoffeeMilk;
+    delete newCoffee;
+
     return 0;
 }
